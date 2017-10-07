3bc213952a3a9e8c943b0be341f793eaf540d008
yonathan <yonathan@DESKTOP-0LGCRF4>
2017-10-06 23:15:22 -0200

    ctrl c correccion

---
+++ StreamingServer/src/uy/edu/fing/redes2017/grupo12/ShutdownHook.java
@@ -0,0 +1,38 @@
+package uy.edu.fing.redes2017.grupo12;
+
+public class ShutdownHook {
+	static ConexionManager cm;
+
+	static ProcessStreaming pf;
+	static SenderFrame sf;
+	static StatadisitcThread sT;
+	
+public ShutdownHook(ConexionManager cm, ProcessStreaming pf,
+			SenderFrame sf, StatadisitcThread sT) {
+	this.cm=cm;
+	this.sf=sf;
+	this.pf=pf;
+	this.sT=sT;
+		// TODO Auto-generated constructor stub
+	}
+
+public void attachShutDownHook() {
+	
+	
+	Runtime.getRuntime().addShutdownHook(new Thread() {
+        @Override
+        public void run() {
+        
+        pf.fin();
+        sf.fin();
+        cm.fin();
+        sT.fin();
+            System.out.println("exit ......");
+        }
+    });
+
+}
+
+
+
+}
